86c64c8864bc95cc1f46609fccd860450f75c358
Jan Nakladal <mojeto1@gmail.com>
2014-12-14 00:44:42 +1300

    sport-activity-list element added

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,6 +4,21 @@
   document.addEventListener('polymer-ready', function() {
     // Perform some behaviour
     console.log('Polymer is ready to rock!');
+    var serverUrl = localStorage.getItem('server-url');
+    console.log("Server url is: " + serverUrl);
+    document.querySelector('sport-activity-list')
+        .url = serverUrl + "/activity";
+
+    var button = document.querySelector("#button");
+    button.addEventListener('click', function() {
+      console.log('click');
+      document.querySelector('sport-activity-list').loadData();
+      //
+    });
+
+
+
+
   });
 
 // wrap document so it plays nice with other libraries
